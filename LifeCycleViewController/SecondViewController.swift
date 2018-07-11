e1ea3bc433b5f483c96c131b463ea55f2b87772a
Alexey Efimov <lex.efimov@gmail.com>
2018-07-11 15:22:23 +0500

    no message

--- LifeCycleViewController/SecondViewController.swift
+++ LifeCycleViewController/SecondViewController.swift
@@ -9,27 +9,95 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    var someProperties: String? {
+        didSet {
+            print("someProrties")
+        }
+    }
+    
+    @IBOutlet weak var closeButton: UIButton! {
+        didSet {
+            print("closeButton")
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    // Срабатывает после загрузки View
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        hello()
+    }
+    
+    // Срабатывает перед появленем вью на экране
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    // Срабатывает перед тем, как размер вью поменятся под размер экрана
+    override func viewWillLayoutSubviews() {
+        print("viewWillLayoutSubviews")
+    }
+    
+    // Тут срабатывает Auto Layout
+    
+    // Срабатывает после того, как размер вью изменился под размер экрана
+    override func viewDidLayoutSubviews() {
+        print("viewDidLayoutSubviews")
+    }
+    
+    override func updateViewConstraints() {
+        print("updateViewConstraints")
+        super.updateViewConstraints()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillTransition(to size: CGSize,
+                                     with coordinator: UIViewControllerTransitionCoordinator) {
+        print("viewWillTransition")
     }
 
+    // Срабатывает при нехватке памяти
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // Обнуление не используемых объектов
+    }
+    
+    func hello() {
+        print("viewDidLoad")
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+    
+    // Срабатывает перед тем, как вью закроется
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    // Срабатывает после закрытия вью
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
+    // Выгружает объект из памяти
+    deinit {
+        print("deinit")
+    }
 
 }
